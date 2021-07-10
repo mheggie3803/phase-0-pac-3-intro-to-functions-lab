c0409fd3834fe08f07d628ab0c1dafdc755723e0
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-07-09 23:25:34 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(hello) {
+    return hello.toUpperCase();
+}
+
+function whisper(HELLO) {
+    return HELLO.toLowerCase();
+}
+
+function logShout(hello) {
+    console.log('HELLO');
+}
+
+function logWhisper(HELLO) {
+    console.log('hello');
+}
+
+function sayHiToGrandma(hello) {
+    return "I can\'t hear you!"
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!");
+}
+
+function sayHiToGrandma(HELLO) {
+    return "YES INDEED!" 
+    console.log(HELLO.toUpperCase());
+}
+
+function sayHiToGrandma(iLoveYouGrandma) {
+    return "I love you, too"
+    console.log(iLoveYouToo === iLoveYouToo)  
+}
